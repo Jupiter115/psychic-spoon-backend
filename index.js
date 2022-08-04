a08d4ca5106ef16caffa3976aaa55f3f1d19cd42
Fred0624 <fred10150912@hotmail.com>
2022-08-04 16:18:38 -0700

    user model/controller connextion index

---
+++ index.js
@@ -0,0 +1,31 @@
+// Dependencies
+const express = require('express');
+const app = express();
+require('./db/connection')
+const cors = require('cors');
+app.set('port', process.env.PORT || 3000)
+// const cors = require('cors')
+
+
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(cors());
+
+// Redirect
+app.get('/', (req, res) => {
+	res.redirect('/products');
+});
+
+// Controllers
+// Forward all requests to localhost:3111/icecreams to the ice cream controller
+const productController = require('./controllers/productController')
+app.use('/products', productController);
+
+const userController = require('./controllers/userController')
+app.use('/users', userController)
+
+app.listen(app.get('port'), () => {
+    console.log('on port:' + app.get('port'))
+})
